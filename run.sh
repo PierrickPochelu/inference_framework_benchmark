7735306a0c312910562949f38b37118c7b0b56fe
PierrickPochelu <44296854+PierrickPochelu@users.noreply.github.com>
2022-10-08 05:23:10 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-LIB_PB_PATH=/data/appli_PITSI/users/pochelu/project/inference_system/models_lib/TF_PB/
-LIB_ONNX_PATH=/data/appli_PITSI/users/pochelu/project/inference_system/models_lib/ONNX/
-LIB_OPENVINO_PATH=/data/appli_PITSI/users/pochelu/project/inference_system/models_lib/OpenVINO/
+LIB_PB_PATH=./models_lib/TF_PB/
+LIB_ONNX_PATH=./models_lib/ONNX/
+LIB_OPENVINO_PATH=./models_lib/OpenVINO/
 
 # FROM PB TO ONNX
 for f in "$LIB_PB_PATH"/*
@@ -12,4 +12,3 @@ pb_dnn_path="${LIB_PB_PATH}${dnn_name}.pb"
 onnx_dnn_path="${LIB_ONNX_PATH}${dnn_name}.onnx"
 ./from_PB_to_ONNX.sh $pb_dnn_path $onnx_dnn_path
 done
-
